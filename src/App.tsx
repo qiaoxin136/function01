c9e9bdf724fffe9a16f879d06864128a9f4ac5a2
qiaoxin136 <qiaoxin136@gmail.com>
2025-12-02 11:08:17 -0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import { generateClient } from "aws-amplify/api"
+import { Schema } from "../amplify/data/resource"
+
+const client = generateClient<Schema>()
+
+async function sayHello() {
+  const result = await client.queries.sayHello({
+      name: 'Barosanu'
+  })
+  console.log(result)
+}
+
+function App() {
+
+
+  return (
+    <>
+      <button onClick={sayHello}>Click me</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import outputs from '../amplify_outputs.json'
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure(outputs);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
